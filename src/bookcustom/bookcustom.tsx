73b2ba76538dbada8dc0b16e4294e097bf6d1d6d
hahahaday12 <hahahaday12@naver.com>
2023-07-03 14:32:49 +0900

    style: 커스텀 한 책 스타일 컴포넌트 추가

---
+++ src/bookcustom/bookcustom.tsx
@@ -0,0 +1,21 @@
+import'./bookcustom.scss'
+
+
+function BookCustom(){
+  return(
+    <>
+    <div id="container">
+      <section className="open-book">
+        <article>
+                
+        </article>   
+      </section>
+    </div>  
+    </>
+  )
+}
+export default BookCustom
+
+
+
+
