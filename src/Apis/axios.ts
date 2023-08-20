4c3ee7575f8788f3052259a031db989f1d2812d7
hahahaday12 <hahahaday12@naver.com>
2023-08-21 01:16:46 +0900

    feat: axiosInstance 생성

---
+++ src/Apis/axios.ts
@@ -0,0 +1,29 @@
+import axios, { AxiosError, AxiosInstance } from 'axios'
+
+
+const authInterceptors = (instance: AxiosInstance): AxiosInstance => {
+    instance.interceptors.request.use(
+      config => {
+        // 로컬스토리지에 저장되어 있는 AccessToken을 호출.
+        const token = localStorage.getItem('token')
+        if (config.headers && token) {
+          // accessToken이 정상적으로 저장 => headers에 authorization 값을 추가.
+          config.headers.Authorization = `Bearer ${token}`
+        }
+        // authorization을 추가한 config 반환
+        return config
+      },
+      (error: AxiosError): Promise<AxiosError> => {
+        return Promise.reject(error)
+      }
+    )
+    return instance
+  };
+
+// Authorization 설정이 없는 일반 사용자 API용 Instance
+export const baseInstance: AxiosInstance = axios.create({
+  baseURL: 'https://asia-northeast3-heropy-api.cloudfunctions.net/api/auth'
+})
+
+// Authorization 설정이 추가된 로그인한 사용자 API용 Instance --유저 API 에서 공통적으로 사용할 인스턴스
+export const authInstance: AxiosInstance = authInterceptors(baseInstance)
