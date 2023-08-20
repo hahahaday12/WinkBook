247c6a6c8723ade540a6b6c7ce6d17592e003890
hahahaday12 <hahahaday12@naver.com>
2023-08-21 01:17:27 +0900

    fix: 설정된 instance 로 수정.

--- src/Apis/register.tsx
+++ src/Apis/register.tsx
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import { authInstance, baseInstance } from './axios';
 
 const { VITE_KDT5_API, VITE_KDT5_USER } = import.meta.env;
 
@@ -8,11 +8,6 @@ const headers = {
   username: VITE_KDT5_USER,
 };
 
-const axiosInstance = axios.create({
-  baseURL: 'https://asia-northeast3-heropy-api.cloudfunctions.net/api/auth',
-  headers,
-});
-
 export const JoinForm = async (
   email: string,
   displayName: string,
@@ -20,7 +15,7 @@ export const JoinForm = async (
   profileImgBase64: string
 ) => {
   const URL = '/signup';
-  const res = await axiosInstance.post(URL, {
+  const res = await baseInstance.post(URL, {
     email,
     displayName,
     password,
@@ -31,28 +26,19 @@ export const JoinForm = async (
 
 export const LoginForm = async (email: string, password: string) => {
   const LOGINURL = '/login';
-  const res = await axiosInstance.post(LOGINURL, { email, password });
+  const res = await baseInstance.post(LOGINURL, { email, password });
   return res.data;
 };
 
 export const LogoutForm = async () => {
   const LOGOUTURL = '/logout';
-  const token = localStorage.getItem('token') as string;
-  const res = await axiosInstance.post(
-    LOGOUTURL,
-    {},
-    { headers: { ...headers, Authorization: `Bearer ${token}` } }
-  );
+  const res = await authInstance.post(LOGOUTURL,{},{ headers });
   return res.data;
 };
 
 export const TokenMe = async () => {
   const AUTHME = '/me';
-  const token = localStorage.getItem('token') as string;
-  const res = await axiosInstance.post(
-    AUTHME,
-    {},
-    { headers: { ...headers, Authorization: `Bearer ${token}` } }
+  const res = await authInstance.post(AUTHME,{},{ headers }
   );
   return res.data;
 };
@@ -64,11 +50,9 @@ export const InfoToken = async (
   newPassword: string
 ) => {
   const InfoMe = '/user';
-  const token = localStorage.getItem('token') as string;
-  const res = await axiosInstance.put(
-    InfoMe,
+  const res = await authInstance.put(InfoMe,
     { displayName, profileImgBase64, oldPassword, newPassword },
-    { headers: { ...headers, Authorization: `Bearer ${token}` } }
+    { headers }
   );
   return res.data;
 };
