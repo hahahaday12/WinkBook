d10381e7b6cfb2636cf3fb4204e3d630a918391f
hahahaday12 <hahahaday12@naver.com>
2023-08-27 23:50:48 +0900

    fix: 사용하지 않는 함수 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-
-const token = localStorage.getItem('token');
-
-// 토큰을 저장하는 함수
-const saveTokenToLocalStorage = (token: string) => {
-  localStorage.setItem('token', token);
-};
-// 토큰을 제거하는 함수
-const removeTokenFromLocalStorage = () => {
-  localStorage.removeItem('token');
-};
-
-if (token) {
-  saveTokenToLocalStorage(token);
-} else {
-  removeTokenFromLocalStorage();
-}
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <App />
 );
