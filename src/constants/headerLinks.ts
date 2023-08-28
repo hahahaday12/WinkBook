7766723ac8171bd4a403aa0645c305a9206e6c9f
hahahaday12 <hahahaday12@naver.com>
2023-08-29 01:10:12 +0900

    refac:  header links  설정

---
+++ src/constants/headerLinks.ts
@@ -0,0 +1,14 @@
+export const links = [
+  { path: '/join', text: '회원가입' },
+  { path: '/login', text: '로그인' }
+]
+
+export const tokenLinks = [
+  { path: '/cart', text: '장바구니' },
+  { path: '/mypage', text: '마이페이지' },
+]
+
+export const headerText = {
+  signout: '로그아웃'
+}
+
