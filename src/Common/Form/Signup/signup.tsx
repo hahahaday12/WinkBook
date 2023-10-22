999640e7a80814dd429374e46c801e90cc2f34dd
hahahaday12 <hahahaday12@naver.com>
2023-10-22 20:02:15 +0900

    Fix: 오류 예외처리 추가 수정

--- src/Common/Form/Signup/signup.tsx
+++ src/Common/Form/Signup/signup.tsx
@@ -1,19 +1,20 @@
-import { FormEvent, useState, useCallback, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { PwCheck, emailCheck } from "../Validation ";
-import { JoinForm } from "@/Apis/register";
-import Swal from "sweetalert2";
-import "./signup.scss";
+import { FormEvent, useState, useCallback, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { PwCheck, emailCheck } from '../Validation ';
+import { JoinForm } from '@/Apis/register';
+import Swal from 'sweetalert2';
+import './signup.scss';
+import axios from 'axios';
 
 function Join() {
   const navigate = useNavigate();
 
   // 이름 , 비밀번호, 이메일 , 비밀번호 확인
-  const [email, setUserEmail] = useState<string>("");
-  const [displayName, setdDisplayName] = useState<string>("");
-  const [password, setUserPassword] = useState<string>("");
-  const [confirmPassword, setConfirmPassword] = useState<string>("");
-  const [profileImgBase64, setProfileImgBase64] = useState<string>("");
+  const [email, setUserEmail] = useState<string>('');
+  const [displayName, setdDisplayName] = useState<string>('');
+  const [password, setUserPassword] = useState<string>('');
+  const [confirmPassword, setConfirmPassword] = useState<string>('');
+  const [profileImgBase64, setProfileImgBase64] = useState<string>('');
 
   //비밀번호 유효성 검사
   const [isName, setIsName] = useState<boolean>(false);
@@ -22,14 +23,20 @@ function Join() {
   const [isPasswordConfirm, setIsPasswordConfirm] = useState<boolean>(false);
 
   //오류 메세지 저장
-  const [nameMessage, setNameMessage] = useState<string>("");
-  const [emailMessage, setEmailMessage] = useState<string>("");
-  const [passwordMessage, setPasswordMessage] = useState<string>("");
-  const [passwordConfirmMessage, setPasswordConfirmMessage] = useState<string>("");
+  const [nameMessage, setNameMessage] = useState<string>('');
+  const [emailMessage, setEmailMessage] = useState<string>('');
+  const [passwordMessage, setPasswordMessage] = useState<string>('');
+  const [passwordConfirmMessage, setPasswordConfirmMessage] =
+    useState<string>('');
 
   useEffect(() => {
     if (confirmPassword) {
-      onChangePasswordConfirm(password, confirmPassword, setPasswordConfirmMessage, setIsPasswordConfirm );
+      onChangePasswordConfirm(
+        password,
+        confirmPassword,
+        setPasswordConfirmMessage,
+        setIsPasswordConfirm
+      );
     }
   }, [password, confirmPassword]);
 
@@ -38,10 +45,10 @@ function Join() {
       const emails = e.target.value;
       setUserEmail(emails);
       if (emailCheck(emails)) {
-        setEmailMessage("올바른 이메일 형식이에요 : )");
+        setEmailMessage('올바른 이메일 형식이에요 : )');
         setIsEmail(true);
       } else {
-        setEmailMessage("이메일 형식이 틀렸습니다");
+        setEmailMessage('이메일 형식이 틀렸습니다');
         setIsEmail(false);
       }
     },
@@ -51,10 +58,10 @@ function Join() {
   const onChangeName = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
     setdDisplayName(e.target.value);
     if (e.target.value.length < 2 || e.target.value.length > 21) {
-      setNameMessage("2글자 이상 21글자 미만으로 입력해주세요.");
+      setNameMessage('2글자 이상 21글자 미만으로 입력해주세요.');
       setIsName(false);
     } else {
-      setNameMessage("올바른 이름 형식입니다 :)");
+      setNameMessage('올바른 이름 형식입니다 :)');
       setIsName(true);
     }
   }, []);
@@ -65,10 +72,10 @@ function Join() {
       setUserPassword(passwordCurrent);
 
       if (!PwCheck(passwordCurrent)) {
-        setPasswordMessage("8자리 이상 입력해주세요.");
+        setPasswordMessage('8자리 이상 입력해주세요.');
         setIsPassword(false);
       } else {
-        setPasswordMessage("안전한 비밀번호에요 : )");
+        setPasswordMessage('안전한 비밀번호에요 : )');
         setIsPassword(true);
       }
     },
@@ -82,10 +89,10 @@ function Join() {
     setIsPasswordConfirm: React.Dispatch<React.SetStateAction<boolean>>
   ) => {
     if (password === confirmPassword) {
-      setPasswordConfirmMessage("비밀번호를 똑같이 입력했어요 : )");
+      setPasswordConfirmMessage('비밀번호를 똑같이 입력했어요 : )');
       setIsPasswordConfirm(true);
     } else {
-      setPasswordConfirmMessage("비밀번호가 틀립니다. 다시 입력해주세요.");
+      setPasswordConfirmMessage('비밀번호가 틀립니다. 다시 입력해주세요.');
       setIsPasswordConfirm(false);
     }
   };
@@ -125,12 +132,12 @@ function Join() {
     event.preventDefault();
 
     if (!email) {
-      Swal.fire("이메일을 입력해주세요!", "", "warning");
+      Swal.fire('이메일을 입력해주세요!', '', 'warning');
       return false;
     }
 
     if (!isEmail || !isPassword || !isPasswordConfirm) {
-      Swal.fire("값이 잘못 되었습니다.", "다시 입력해주세요", "warning");
+      Swal.fire('값이 잘못 되었습니다.', '다시 입력해주세요', 'warning');
       return false;
     }
 
@@ -143,27 +150,23 @@ function Join() {
       );
 
       if (res.accessToken) {
-        Swal.fire("가입되었습니다!", "찾아주셔서 감사합니다:)", "success").then(
+        Swal.fire('가입되었습니다!', '찾아주셔서 감사합니다:)', 'success').then(
           () => {
-            navigate("/login");
+            navigate('/login');
           }
         );
       } else {
-        Swal.fire("가입에 실패했습니다.", "다시 시도해주세요", "error");
+        Swal.fire('가입에 실패했습니다.', '다시 시도해주세요', 'error');
       }
     } catch (error) {
-      if (error instanceof Error) {
-        if (error.message === "401") {
-          Swal.fire("중복된 아이디입니다.", "", "warning");
-        } else {
-          console.error("오류가 발생했습니다:", error);
-        }
+      if (axios.isAxiosError(error) && error.response && error.response.data) {
+        const errorMessage = error.response.data;
+        Swal.fire(errorMessage, '다시 시도해주세요', 'error');
       } else {
-        console.error("오류가 발생했습니다:", error);
+        console.error('오류가 발생했습니다:', error);
       }
     }
   }
-
 
   return (
     <>
@@ -178,12 +181,12 @@ function Join() {
               <input
                 placeholder="이메일을 입력하세요"
                 autoComplete="off"
-                type={"text"}
-                name={"email"}
+                type={'text'}
+                name={'email'}
                 onChange={onChangeEmail}
               />
               {email.length > 0 && (
-                <span className={`message ${isEmail ? "success" : "error"}`}>
+                <span className={`message ${isEmail ? 'success' : 'error'}`}>
                   {emailMessage}
                 </span>
               )}
@@ -193,12 +196,12 @@ function Join() {
               <input
                 placeholder="이름을 입력하세요"
                 autoComplete="off"
-                type={"text"}
-                name={"name"}
+                type={'text'}
+                name={'name'}
                 onChange={onChangeName}
               />
               {displayName.length > 0 && (
-                <span className={`message ${isName ? "success" : "error"}`}>
+                <span className={`message ${isName ? 'success' : 'error'}`}>
                   {nameMessage}
                 </span>
               )}
@@ -208,12 +211,12 @@ function Join() {
               <input
                 placeholder="비밀번호를 입력하세요"
                 autoComplete="off"
-                type={"password"}
-                name={"password"}
+                type={'password'}
+                name={'password'}
                 onChange={onChangePassword}
               />
               {password.length > 0 && (
-                <span className={`message ${isPassword ? "success" : "error"}`}>
+                <span className={`message ${isPassword ? 'success' : 'error'}`}>
                   {passwordMessage}
                 </span>
               )}
@@ -223,14 +226,14 @@ function Join() {
               <input
                 placeholder="비밀번호를 한번 더 입력해주세요"
                 autoComplete="off"
-                type={"password"}
-                name={"passwordConfirm"}
+                type={'password'}
+                name={'passwordConfirm'}
                 onChange={onPasswordConfirmChange}
               />
               {confirmPassword.length > 0 && (
                 <span
                   className={`message ${
-                    isPasswordConfirm ? "success" : "error"
+                    isPasswordConfirm ? 'success' : 'error'
                   }`}
                 >
                   {passwordConfirmMessage}
@@ -255,7 +258,10 @@ function Join() {
               <button
                 className="buttonBox"
                 type="submit"
-                disabled={!(isName && isEmail && isPassword && isPasswordConfirm)}>
+                disabled={
+                  !(isName && isEmail && isPassword && isPasswordConfirm)
+                }
+              >
                 등록
               </button>
             </div>
