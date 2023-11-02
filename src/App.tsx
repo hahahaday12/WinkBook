efa6ab9c1e1d4089d1fa447dcd54d8ad6ecd0ded
hahahaday12 <hahahaday12@naver.com>
2023-11-03 03:01:12 +0900

    Remove: 사용되지 않은 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,22 @@ import './App.scss';
 import RoutesPage from './routes/routes';
 import { TokenContext } from './Context/TokenContext';
 import { useState } from 'react';
-import { RecoilRoot } from 'recoil';
 
 function App() {
-
   const [token, setTokenState] = useState(localStorage.getItem('token'));
 
   const setToken = (newToken: string) => {
     setTokenState(newToken);
     window.localStorage.setItem('token', newToken);
-  }
-  
+  };
+
   return (
     <>
-    <RecoilRoot>
-    <TokenContext.Provider value={{ token, setToken }}>
-      <RoutesPage />
-    </TokenContext.Provider>
-    </RecoilRoot>
+      <TokenContext.Provider value={{ token, setToken }}>
+        <RoutesPage />
+      </TokenContext.Provider>
     </>
   );
 }
-
 
 export default App;
